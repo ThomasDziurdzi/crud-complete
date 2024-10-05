90cb2fcc226e2a9493b2c883ded6e123ce6f62e3
ThomasDziurdzi <thomasdziurdzi@gmail.com>
2024-10-05 17:17:23 +0200

    modification on movie router

--- server/src/routes/movieRoutes.js
+++ server/src/routes/movieRoutes.js
@@ -1,15 +1,15 @@
-import express from "express"
-import * as movieControllers from "../controllers/movieController.js"
+import express from "express";
+import * as movieControllers from "../controllers/movieController.js";
+import upload from "../middlewares/multer.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/", movieControllers.createMovie)
+router.post("/", upload.single("image"), movieControllers.createMovie);
 
-router.get("/", movieControllers.readAllMovie)
+router.get("/", movieControllers.readAllMovie);
 
-router.put("/:id", movieControllers.updateMovie)
+router.put("/:id", movieControllers.updateMovie);
 
-router.delete("/:id", movieControllers.deleteMovie)
+router.delete("/:id", movieControllers.deleteMovie);
 
 export default router;
-
